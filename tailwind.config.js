c2f153636b799e2d97f22001a50df02b1a754d0c
arirzwn <aririzwan0@gmail.com>
2023-08-24 12:57:00 +0700

    Upload code

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#35CAA4',
+      },
+      fontFamily: {
+        'curvsive': ['Forum', 'cursive;'],
+      },
+      listStyleImage: {
+        checkmark: 'url("/public/assets/check.svg")',
+      },
+    },
+  },
+  plugins: [],
+}
