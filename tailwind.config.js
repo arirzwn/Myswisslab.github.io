7310cd07a8c64bec57ff655f8d4c138b2541e17c
arirzwn <aririzwan0@gmail.com>
2024-03-13 14:50:42 +0700

    Update path css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./public/**/*.{html,js}',
+    "./src/**/*.{css}",],
   theme: {
     extend: {
       colors: {
